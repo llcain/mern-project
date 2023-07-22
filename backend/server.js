f0318af56f7140e82be45c00c03c562a68249d1c
llcain <llcain2012@gmail.com>
2023-07-22 12:59:19 -0500

    created .gitignore file

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 // Creating an express server
-
 const express = require('express');
 
 // express app
 const app = express();
 
-// listen for request
+// routes 
+app.get('/', (req, res) => {
+    res.json({mssg: "Welcome to the app"})
+});
 
+// listen for request
 app.listen(4000, () => {
     console.log("listening on port 4000!!")
 });
